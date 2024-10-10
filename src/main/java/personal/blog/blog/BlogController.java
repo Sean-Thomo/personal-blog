1276d57abc959e05cd3a2bcd2f9935e51c31f346
Siphesihle-Thomo <seansthomo@gmail.com>
2024-10-10 21:08:08 +0200

    FEAT: create signup html template

--- src/main/java/personal/blog/blog/BlogController.java
+++ src/main/java/personal/blog/blog/BlogController.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 
 @Controller
 public class BlogController {
+    private HashMap<String, String> userCredentials = new HashMap<>();
 
     private static final String USERNAME = "seansthomo@gmail.com";
     private static final String PASSWORD = "password123";
@@ -41,13 +42,11 @@ public class BlogController {
 
     @PostMapping("/login")
     public String loginPost(@RequestParam String email, @RequestParam String password) {
-        if (USERNAME.equals(email) && PASSWORD.equals(password)) {
+        if (userCredentials.containsKey(email) && userCredentials.containsKey(password)) {
             System.out.println("Login successful for user: " + email);
             return "dashboard";
         } else {
-            // Invalid credentials
-            System.out.println("Login failed for user: " + email);
-            return "login";
+            return "signup";
         }
     }
 }
