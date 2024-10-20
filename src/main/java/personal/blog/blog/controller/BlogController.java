020422a6d309746dd97b981d667b9e79d5f8c85a
Siphesihle-Thomo <seansthomo@gmail.com>
2024-10-20 19:17:58 +0200

    FEAT: add Autowired annotation

--- src/main/java/personal/blog/blog/controller/BlogController.java
+++ src/main/java/personal/blog/blog/controller/BlogController.java
@@ -1,5 +1,6 @@
 package personal.blog.blog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Controller
 public class BlogController {
+    @Autowired
     BlogService blogService;
 
     @GetMapping("/")
