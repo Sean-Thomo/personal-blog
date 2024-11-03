a187c9daba9e07bc99919086ba50404c3ead37b5
Siphesihle-Thomo <seansthomo@gmail.com>
2024-11-03 14:18:39 +0200

    FEAT: create new entity Writer.java

--- src/main/java/personal/blog/blog/entity/Article.java
+++ src/main/java/personal/blog/blog/entity/Article.java
@@ -1,11 +1,9 @@
 package personal.blog.blog.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Articles")
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
